d19e4b02cf2a406b8d808b899343676438af45cc
Vinicius Prado <viniciusprado99@gmail.com>
2026-04-06 15:55:12 -0300

    Versão Final

--- README.md
+++ README.md
@@ -11,17 +11,17 @@
 * [X] Cores diferentes para round, descanso e pra quando acabar o ciclo (talvez com aviso sonoro também)
  
 ## 2. Estado da Aplicação (O que o sistema precisa de "lembrar" - Variáveis JS)
-* [ ] O tempo total do round (ex: 180 segundos)
-* [ ] O tempo que falta no momento
-* [ ] Quantos rounds foram feitos
+* [X] O tempo total do round (ex: 180 segundos)
+* [X] O tempo que falta no momento
+* [X] Quantos rounds foram feitos
 
 
 ## 3. Comportamentos (O que acontece e quando - Eventos/Funções)
 * [X] Quando o utilizador clicar em "Iniciar": O cronometro inicia, indica qual round está, a tela fica branca com as letras pretas
 * [X] Quando o utilizador clicar em "Parar": O cronometro para e a tela fica azul com as letras pretas, e apenas os numeros do timer ficam piscando
-* [ ] Quando o utilizador clicar em "Reiniciar": O cronometro é zerado, reinicia a contagem dos rounds
-* [ ] Quando o utilizador clicar em "Finalizar": Volta para a tela inicial de escolha e zera tudo
-* [ ] Quando o utilizador selecionar a quantidade de rounds, tempo de round e descanso: os visores devem ser alterados
+* [X] Quando o utilizador clicar em "Reiniciar": O cronometro é zerado, reinicia a contagem dos rounds
+* [X] Quando o utilizador clicar em "Finalizar": Volta para a tela inicial de escolha e zera tudo
+* [X] Quando o utilizador selecionar a quantidade de rounds, tempo de round e descanso: os visores devem ser alterados
 * [X] Quando o tempo chegar a zero: O cronometro para e uma tela fica piscando em vermelho zerando o tempo, mas deixando a quantidade de rounds feitos a mostra
 
 ## 4. Futuras implementações
--- assets/script.js
+++ assets/script.js
@@ -12,24 +12,33 @@ let selecDescanso = document.querySelector("#rest")
 let iniciar = document.querySelector ("#startBtn");
 let pausar = document.querySelector ("#pauseBtn");
 let reset = document.querySelector ("#resetBtn");
-let finalizar = document.querySelector ("#finishBtn");
+let finish = document.querySelector ("#finishBtn");
 
 //Variáveis de controle
 let tempoRestante = 0;
-let roundAtual = 0;
+let roundAtual = 1;
 let intervaloId = null;
-
-
-
-
+let emDescanso = false;
 
 //Eventos dos botões
 iniciar.addEventListener("click", inicio);
 pausar.addEventListener("click", pausa);
 reset.addEventListener("click", resetar);
-finalizar.addEventListener("click", finalizar);
+finish.addEventListener("click", finalizar);
 
 //Funções
+
+function atualizarVisor(){
+    //calcula os minutos e segundos exatos desse momento
+    let minutos = Math.floor(tempoRestante / 60);
+    let segundos = tempoRestante % 60;
+
+    // Trasforma o numero em string e formata para sempre ter 2 digitos, preenchendo com 0 se faltar
+    let minFormatado = String(minutos).padStart(2, "0"); //Formata os minutos para ter sempre 2 dígitos
+    let segFormatado = String(segundos).padStart(2, "0"); //Formata os segundos para ter sempre 2 dígitos
+
+    visorTempo.textContent = `${minFormatado}:${segFormatado}`;
+}
 
 function inicio(){
     //Limpa qualquer intervalo que esteja rodando e volta a tela a cor original
@@ -37,32 +46,60 @@ function inicio(){
     document.body.classList.remove("tela-pausada");
 
     if(tempoRestante === 0){
-        // Pega o tempo do select
-    tempoRestante = parseInt(selecTempo.value);
+        if (emDescanso) {
+            tempoRestante = parseInt(selecDescanso.value);
+        } else {
+             // Pega o tempo do select
+             tempoRestante = parseInt(selecTempo.value);
+        }    
     }
     
     // Roda o bloco de código a cada 1 segundo
     intervaloId = setInterval(function (){
         if(tempoRestante > 0){
             tempoRestante--;
-
-            //calcula os minutos e segundos exatos desse momento
-            let minutos = Math.floor(tempoRestante / 60);
-            let segundos = tempoRestante % 60;
-
-            // Trasforma o numero em string e formata para sempre ter 2 digitos, preenchendo com 0 se faltar
-            let minFormatado = String(minutos).padStart(2, "0"); //Formata os minutos para ter sempre 2 dígitos
-            let segFormatado = String(segundos).padStart(2, "0"); //Formata os segundos para ter sempre 2 dígitos
-
-            visorTempo.textContent = `${minFormatado}:${segFormatado}`;
+            atualizarVisor();
         } else {
-            clearInterval(intervaloId);
-            document.body.classList.add("tela-piscando-vermelho")
+            if (emDescanso === true) {
+                tempoRestante = parseInt(selecTempo.value);
+                emDescanso = false;
+                document.body.classList.remove("tela-descanso");
+                atualizarVisor();
+            } else if (emDescanso === false && roundAtual < parseInt(selecRounds.value)){
+                roundAtual++;
+                tempoRestante = parseInt(selecDescanso.value);
+                visorRound.textContent = roundAtual;
+                emDescanso = true;
+                document.body.classList.add("tela-descanso");
+                atualizarVisor();
+            } else {
+                clearInterval(intervaloId);
+                document.body.classList.add("tela-piscando-vermelho")
+            }
         }
     }, 1000); //em milissegundos
+
+    visorRound.textContent = roundAtual;
+    totalRounds.textContent = selecRounds.value;
 }
 
 function pausa(){
     clearInterval(intervaloId);
     document.body.classList.add("tela-pausada");
+}
+
+function resetar(){
+    clearInterval(intervaloId);
+    tempoRestante = 0;
+    emDescanso = false;
+    visorTempo.textContent = "00:00";
+    document.body.classList.remove("tela-pausada", "tela-piscando-vermelho");
+}
+
+function finalizar(){
+    resetar();
+    roundAtual = 1;
+    visorRound.textContent = "1";
+    totalRounds.textContent = selecRounds.value;
+   
 }
